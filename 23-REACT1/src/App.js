46bd693b01a917282a09249566962c9c366d8a60
이진아 <mnb2098@naver.com>
2023-03-30 11:13:15 +0900

    tick.html 추가

--- 23-REACT1/src/App.js
+++ 23-REACT1/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+//3/30 tick예제 
+function Clock() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>안녕, 리액트?</h1>
+      <h2>현재시간: {new Date().toLocaleTimeString()}</h2>
     </div>
   );
 }
+function tick() {
+  // eslint-disable-next-line no-undef
+  ReactDOM.render(<Clock />, document.getElementById("root"));
+}
 
-export default App;
+setInterval(tick, 1000);
+
+export default Clock;
