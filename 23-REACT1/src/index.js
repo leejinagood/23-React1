a3cf57954f4806139afd4d3bd3062e1d2883ed16
이진아 <mnb2098@naver.com>
2023-06-07 14:14:47 +0900

    .

--- 23-REACT1/src/index.js
+++ 23-REACT1/src/index.js
@@ -25,7 +25,7 @@ import Blocks from './chapter15/Blocks';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Blocks />
+    <DarkOrLight />
   </React.StrictMode>
 );
 
